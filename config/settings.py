378b43583bf909fbf803ff047775231c483a1d4d
goeasyonng <song@songui-MacBookAir.local>
2023-07-20 15:49:53 +0900

    setting change

--- config/settings.py
+++ config/settings.py
@@ -19,14 +19,7 @@ pymysql.install_as_MySQLdb()
 
 env = environ.Env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
@@ -34,13 +27,8 @@ SECRET_KEY = env("SECRET_KEY")
 CF_TOKEN = env("CF_TOKEN")
 CF_ID = env("CF_ID")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False if os.environ.get("SERVER") == "NAVER" else True
-# DEBUG = os.environ.get("DEPLOY") != "DEPLOY"
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
-
-# Application definition
 
 THIRD_PARTY_APPS = [
     "corsheaders",
@@ -108,14 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -131,10 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "ko-KR"
 
 TIME_ZONE = "Asia/Seoul"
@@ -142,10 +118,6 @@ TIME_ZONE = "Asia/Seoul"
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
 
@@ -191,12 +163,7 @@ if os.environ.get("SERVER") == "NAVER":
         integrations=[
             DjangoIntegration(),
         ],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
         traces_sample_rate=1.0,
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
         send_default_pii=True,
     )
 
